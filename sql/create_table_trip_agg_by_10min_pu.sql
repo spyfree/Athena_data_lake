d9d31ddc441e5accb4d2d108f9ab75733dd81d8d
spyfree <srlixin@gmail.com>
2020-06-02 00:03:28 +0800

    update some create sql ddl

---
+++ sql/create_table_trip_agg_by_10min_pu.sql
@@ -0,0 +1,37 @@
+CREATE EXTERNAL TABLE `trip_agg_by_10min_pu`(
+  `trip_taxi_type` string, 
+  `vendorid` bigint, 
+  `pickup_date` date, 
+  `pickup_timestamp` timestamp, 
+  `trip_count` bigint, 
+  `trip_duration_min` double, 
+  `passenger_count` bigint, 
+  `trip_distance` double, 
+  `pickup_locationid` int, 
+  `pickup_zone` string, 
+  `pickup_borough` string, 
+  `payment_type` bigint, 
+  `fare_amount` double, 
+  `total_amount` double, 
+  `extra` double, 
+  `mta_tax` double, 
+  `tip_amount` double, 
+  `tolls_amount` double, 
+  `improvement_surcharge` double, 
+  `congestion_surcharge` double, 
+  `is_fog` int, 
+  `is_rain` int, 
+  `is_snow` int, 
+  `tmax` bigint, 
+  `tmin` bigint, 
+  `prcp` double)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://trip-ny-parquet/trip_agg_by_10min_pu'
+TBLPROPERTIES (
+  'parquet.compress'='GZIP')
---
+++ sql/create_table_trip_yellow.sql
@@ -0,0 +1,35 @@
+CREATE EXTERNAL TABLE `trip_yellow`(
+  `vendorid` bigint, 
+  `pickup_datetime` string, 
+  `dropoff_datetime` string, 
+  `passenger_count` bigint, 
+  `trip_distance` double, 
+  `ratecodeid` bigint, 
+  `store_and_fwd_flag` string, 
+  `pickup_longitude` double, 
+  `pickup_latitude` double, 
+  `pickup_locationid` int, 
+  `dropoff_longitude` double, 
+  `dropoff_latitude` double, 
+  `dropoff_locationid` int, 
+  `payment_type` bigint, 
+  `fare_amount` double, 
+  `extra` double, 
+  `mta_tax` double, 
+  `tip_amount` double, 
+  `tolls_amount` double, 
+  `improvement_surcharge` double, 
+  `total_amount` double, 
+  `congestion_surcharge` double)
+PARTITIONED BY ( 
+  `month` string)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://trip-ny-parquet/yellow'
+TBLPROPERTIES (
+  'parquet.compress'='GZIP') 
